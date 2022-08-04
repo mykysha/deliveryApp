506fcd08ff7cf811cbd0c7fb03515061f9c2e670
Shohrukh Kholov <shohrukh_kholov@epam.com>
2022-08-04 14:04:59 +0300

    added grpc handlers

--- app/services/order/api/v1/grpclogic/grpclogic.go
+++ app/services/order/api/v1/grpclogic/grpclogic.go
@@ -5,9 +5,11 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/nndergunov/deliveryApp/app/services/order/api/v1/grpclogic/pb"
-	"github.com/nndergunov/deliveryApp/app/services/order/pkg/service"
 	"google.golang.org/grpc"
+
+	"github.com/nndergunov/deliveryApp/app/services/order/api/v1/grpclogic/pb"
+	"github.com/nndergunov/deliveryApp/app/services/order/pkg/domain"
+	"github.com/nndergunov/deliveryApp/app/services/order/pkg/service"
 )
 
 type orderGRPC struct {
@@ -46,6 +48,52 @@ func (r *orderGRPC) GetOrder(ctx context.Context, req *pb.Request) (*pb.OrderRes
 		FromUserID:   int32(order.FromUserID),
 		RestaurantID: int32(order.RestaurantID),
 		OrderItems:   items,
-		Status:       order.Status.Status,
+		Status: &pb.OrderStatus{
+			OrderID: int32(order.Status.OrderID),
+			Status:  order.Status.Status,
+		},
 	}, nil
+}
+
+// GetOrderList handles request of getting the order from the service.
+func (r *orderGRPC) GetOrderList(ctx context.Context, req *pb.Parameters) (*pb.OrderResponseList, error) {
+	i := int(req.FromRestaurantID)
+	param := domain.SearchParameters{
+		FromRestaurantID: &i,
+		Statuses:         req.Statuses,
+		ExcludeStatuses:  req.ExcludeStatuses,
+	}
+	orderList, err := r.service.ReturnOrderList(param)
+	if err != nil {
+		return nil, fmt.Errorf("getting order list: %w", err)
+	}
+
+	var orderResponseList []*pb.OrderResponse
+	for _, order := range orderList {
+		orderResponse := &pb.OrderResponse{
+			OrderID:      int32(order.OrderID),
+			FromUserID:   int32(order.FromUserID),
+			RestaurantID: int32(order.RestaurantID),
+			OrderItems:   convertSliceToInt32(order.OrderItems),
+			Status: &pb.OrderStatus{
+				OrderID: int32(order.Status.OrderID),
+				Status:  order.Status.Status,
+			},
+		}
+		orderResponseList = append(orderResponseList, orderResponse)
+	}
+
+	return &pb.OrderResponseList{
+		OrderResponseList: orderResponseList,
+	}, nil
+}
+
+func convertSliceToInt32(list []int) []int32 {
+	list32 := make([]int32, len(list))
+
+	for i := range list32 {
+		i32 := int32(i)
+		list32 = append(list32, i32)
+	}
+	return list32
 }
